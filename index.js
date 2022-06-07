00598bb4dfbf1e4ab9f42024090c6655bc9f47fb
Justy116 <giustinogragnaniello116@gmail.com>
2022-06-07 10:10:22 +0200

    add updates

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 let containerCard = document.querySelector(".container__card");
+
 function showPokemon() {
   for (i = 1; i <= 151; i++) {
     fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
       .then((res) => res.json())
       .then((data) => {
         containerCard.innerHTML +=
-         ` 
-            <div class="card ${data.types[0].type.name}">
+           `<div class="card ${data.types[0].type.name}">
                 <img src="${data.sprites.front_default}" alt="">
                 <div class="container__text">
                     <span>#${data.id}</span>
